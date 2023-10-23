a6939b85019cdfe7e23f2b188e20cfe49b1ca85e
Michal Tolek <michal.tolek@consultant.volvo.com>
2023-10-23 15:00:19 +0200

    fix red build;

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -88,9 +88,10 @@ namespace ImageFunctions
                     {
                         var thumbnailWidth = Convert.ToInt32(Environment.GetEnvironmentVariable("THUMBNAIL_WIDTH"));
                         var thumbContainerName = Environment.GetEnvironmentVariable("THUMBNAIL_CONTAINER_NAME");
+                        var thumbPostfixFilename = Environment.GetEnvironmentVariable("THUMBNAIL_POSTFIX_FILENAME") ?? "-thumbnail";
                         var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
                         var blobContainerClient = blobServiceClient.GetBlobContainerClient(thumbContainerName);
-                        var blobName = GetBlobNameFromUrl(createdEvent.Url).Replace(extension, '-thumbnail' + extension);
+                        var blobName = GetBlobNameFromUrl(createdEvent.Url).Replace(extension, thumbPostfixFilename + extension);
 
                         using (var output = new MemoryStream())
                         using (Image image = Image.Load(input))
